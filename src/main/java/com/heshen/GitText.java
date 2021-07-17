809ba422173d25d9aa816d0392b68512e8a309bd
heshen1220 <87534859+heshen1220@users.noreply.github.com>
2021-07-17 14:41:59 +0800

    pull commit

--- src/main/java/com/heshen/GitText.java
+++ src/main/java/com/heshen/GitText.java
@@ -13,5 +13,7 @@ public class GitText {
         System.out.println("master test!");
         System.out.println("hot-fix test!");
         System.out.println("push test!");
+        System.out.println("pull test!");
+        
     }
 }
