7774f6451cc7bca567347e1b53adba911749183c
heshen <929686169@qq.com>
2021-07-17 13:29:25 +0800

    second commit

--- src/main/java/com/heshen/GitText.java
+++ src/main/java/com/heshen/GitText.java
@@ -6,6 +6,7 @@ package com.heshen;
  */
 public class GitText {
     public static void main(String[] args) {
-        System.out.println("Hello git");
+        System.out.println("Hello git2!");
+        System.out.println("Hello git2!");
     }
 }
