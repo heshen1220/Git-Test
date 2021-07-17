146da7915dd26e8ea0546226dcbfbcc17b596f05
heshen <929686169@qq.com>
2021-07-17 13:27:38 +0800

    first commit

---
+++ src/main/java/com/heshen/GitText.java
@@ -0,0 +1,11 @@
+package com.heshen;
+
+/**
+ * @author heshen
+ * @data 2021 - 07 - 17 - 13:20
+ */
+public class GitText {
+    public static void main(String[] args) {
+        System.out.println("Hello git");
+    }
+}
